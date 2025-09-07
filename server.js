cd3c0e0e5974eedb4a6db0ad4a262a6ea9c43206
clane83 <clane83@yahoo.com>
2025-09-06 20:40:45 -0700

    fix server issues

--- server.js
+++ server.js
@@ -5,11 +5,11 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+require("dotenv").config();
+
+const app = express();
 
 
 /* ***********************
@@ -25,21 +25,21 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(express.static("public")) // set static folder
 //Index route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "0.0.0.0";
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`app listening on ${host}:${port}`)
-})
+});
